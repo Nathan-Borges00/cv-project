54cc0a2a54e195d52e25108fc15d6b5c5dc41969
Nathan Ferreira Borges <borges3141@gmail.com>
2023-04-18 17:36:36 -0300

    feat: continuacao implementacao parte direita do curriculo

--- src/app/my-curriculum/my-curriculum.component.ts
+++ src/app/my-curriculum/my-curriculum.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-my-curriculum',
     templateUrl: './my-curriculum.component.html',
-    styleUrls: ['./my-curriculum.component.css'],
+    styleUrls: ['./my-curriculum.component.scss'],
 })
 export class MyCurriculumComponent implements OnInit {
-    public imageUrl: any;
-
-    constructor(private sanitizer: DomSanitizer) {}
+    constructor() {}
 
     ngOnInit() {}
 }
