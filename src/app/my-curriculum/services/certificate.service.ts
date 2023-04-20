0fc9401583f242f7b525fdac1e38c1941877821a
Nathan Ferreira Borges <borges3141@gmail.com>
2023-04-19 23:32:54 -0300

    feat: inicio inclusao dos diplomas

--- src/app/my-curriculum/services/certificate.service.ts
+++ src/app/my-curriculum/services/certificate.service.ts
@@ -7,29 +7,29 @@ import { Certificate } from '../interfaces/certificate';
 export class CertificateService {
     certificates: Certificate[] = [
         {
-            name: 'Certificado conclusão teste nível de inglês',
-            image: '../assets/imgs/certificadoTesteNivelInglesCIT.jpeg',
-            pdfPath: '../assets/certifications/CertificadoInglesCIT.pdf',
+            name: 'Diploma faculdade UNIP',
+            image: '../assets/certifications/diplomaFaculdadeUnip.jpeg',
+            pdfPath: '../assets/certifications/DiplomaFaculdade.pdf',
         },
         {
             name: 'Certificado conclusão teste nível de inglês',
-            image: '../assets/imgs/certificadoTesteNivelInglesCIT.jpeg',
+            image: '../assets/certifications/certificadoTesteNivelInglesCIT.jpeg',
             pdfPath: '../assets/certifications/CertificadoInglesCIT.pdf',
         },
         {
-            name: 'Certificado conclusão teste nível de inglês',
-            image: '../assets/imgs/certificadoTesteNivelInglesCIT.jpeg',
-            pdfPath: '../assets/certifications/CertificadoInglesCIT.pdf',
+            name: 'Certificado Introdução a linguagem JavaScript',
+            image: '../assets/certifications/certificadoJavaScript.jpeg',
+            pdfPath: '../assets/certifications/IntroduçãolinguagemJavaScript.pdf',
         },
         {
             name: 'Certificado conclusão teste nível de inglês',
-            image: '../assets/imgs/certificadoTesteNivelInglesCIT.jpeg',
-            pdfPath: '../assets/certifications/CertificadoInglesCIT.pdf',
+            image: '../assets/certifications/',
+            pdfPath: '../assets/certifications/',
         },
         {
             name: 'Certificado conclusão teste nível de inglês',
-            image: '../assets/imgs/certificadoTesteNivelInglesCIT.jpeg',
-            pdfPath: '../assets/certifications/CertificadoInglesCIT.pdf',
+            image: '../assets/certifications/',
+            pdfPath: '../assets/certifications/',
         },
     ];
 
