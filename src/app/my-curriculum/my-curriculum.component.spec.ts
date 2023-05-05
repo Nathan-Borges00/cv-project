6139424778cb5dc568a8e91ad9253c4f3a65fadb
Nathan Ferreira Borges <borges3141@gmail.com>
2023-05-05 18:00:46 -0300

    feat: testes unitarios

--- src/app/my-curriculum/my-curriculum.component.spec.ts
+++ src/app/my-curriculum/my-curriculum.component.spec.ts
@@ -1,27 +1,50 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { MyCurriculumComponent } from './my-curriculum.component';
+import { CertificateService } from './services/certificate.service';
+import { Certificate } from './interfaces/certificate';
 
 describe('MyCurriculumComponent', () => {
     let component: MyCurriculumComponent;
     let fixture: ComponentFixture<MyCurriculumComponent>;
+    let certificateService: CertificateService;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
             declarations: [MyCurriculumComponent],
+            providers: [CertificateService],
         }).compileComponents();
-    }));
+    });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(MyCurriculumComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
+        certificateService = TestBed.inject(CertificateService);
     });
 
-    it('should create', () => {
+    it('should create the component', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should retrieve the certificates', () => {
+        const certificates: Certificate[] = [{ name: 'teste', image: 'teste.png', pdfPath: '/src/teste' }];
+        spyOn(certificateService, 'getCertificates').and.returnValue(certificates);
+        component.ngOnInit();
+        expect(component.certificates).toEqual(certificates);
+    });
+
+    it('should open a PDF in a new tab', () => {
+        spyOn(window, 'open');
+        component.openPdf('http://example.com/certificate.pdf');
+        expect(window.open).toHaveBeenCalledWith('http://example.com/certificate.pdf', '_blank');
+    });
+
+    it('should display a page load message', () => {
+        const element = document.createElement('div');
+        element.setAttribute('id', 'status-message');
+        spyOn(document, 'getElementById').and.returnValue(element);
+        component.loadPageMassage();
+        expect(element.textContent).toEqual('Curriculo de Nathan Ferreira Borges carregado com sucesso!');
     });
 });
--- src/app/my-curriculum/my-curriculum.component.ts
+++ src/app/my-curriculum/my-curriculum.component.ts
@@ -7,12 +7,6 @@ import { faCakeCandles } from '@fortawesome/free-solid-svg-icons';
     selector: 'app-my-curriculum',
     templateUrl: './my-curriculum.component.html',
     styleUrls: ['./my-curriculum.component.scss'],
-    template: `
-        <h3 id="typing-nome" class="my-3 text-center typing">
-            <b>{{ nome }}</b>
-        </h3>
-        <h4 id="typing-profissao" class="m-0 typing">{{ profissao }}</h4>
-    `,
 })
 export class MyCurriculumComponent implements OnInit {
     cakeCandles = faCakeCandles;
