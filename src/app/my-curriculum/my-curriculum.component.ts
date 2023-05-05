069a79c8e1b86810cde614a7e8251c634ba27c90
Nathan Ferreira Borges <borges3141@gmail.com>
2023-05-05 17:42:05 -0300

    feat: alteracao icone de aniversario para ficar mais visivel

--- src/app/my-curriculum/my-curriculum.component.ts
+++ src/app/my-curriculum/my-curriculum.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CertificateService } from './services/certificate.service';
 import { Certificate } from './interfaces/certificate';
+import { faCakeCandles } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-my-curriculum',
     templateUrl: './my-curriculum.component.html',
     styleUrls: ['./my-curriculum.component.scss'],
+    template: `
+        <h3 id="typing-nome" class="my-3 text-center typing">
+            <b>{{ nome }}</b>
+        </h3>
+        <h4 id="typing-profissao" class="m-0 typing">{{ profissao }}</h4>
+    `,
 })
 export class MyCurriculumComponent implements OnInit {
+    cakeCandles = faCakeCandles;
+
     certificates: Certificate[] = [];
 
     constructor(private certificateService: CertificateService) {}
